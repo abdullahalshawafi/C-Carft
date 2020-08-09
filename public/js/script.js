fcc4456235cc781329a2f4840149ac535b05a90f
abdullahalshawafi <abdullahadel.aam@gmail.com>
2020-08-09 16:26:55 +0200

    Started the work on reseting profile pictures

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,13 @@
 const profile_picture = document.querySelector('.user-image');
-const image_input = document.querySelector('#userImage');
-image_input.addEventListener('change', (event) => {
+
+document.querySelector('#userImage').addEventListener('change', (event) => {
     if (event.target.files.length) {
         profile_picture.src = URL.createObjectURL(event.target.files[0]);
     }
+});
+
+document.querySelector('.reset-profile-picture').addEventListener('click', () => {
+    const user_gender = document.querySelector('#gender').value;
+    document.querySelector('#userImage').value = 'default-' + user_gender + '.jpg';
+    profile_picture.src = '/images/profile pictures/default-' + user_gender + '.jpg';
 });
